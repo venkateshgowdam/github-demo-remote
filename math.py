5ad9dc5f28d4c3c42433fe9802364cb7e6072ee2
venkateshgowdam <venkatesh_m@persistent.com>
2022-02-24 00:31:15 +0530

    changed the header fun

--- math.py
+++ math.py
@@ -1,18 +1,12 @@
-#add implementation
+#addition implementation
 def add(x,y):
-    return x+y
-#sub implementation
+    pass
+#substraction implementation
 def sub(x,y):
-    return x+y
-#mul implementation
+    pass
+#multiplication implementation
 def mul(x,y):
-    return x*y #on Bug456 branch
-#div implementation
+    pass
+#division implementation
 def div(x,y):
-    if y==0:		#on master branch
-      return DIVIDE_BY O_ERROR
-    else:
-      return x/y
-#square implemenation
-def square(x):
-     return x*x
+    pass
