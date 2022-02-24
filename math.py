d1fcd11ca4721771a1a44b28ad31ce6c3fe54c03
venkateshgowdam <99476992+venkateshgowdam@users.noreply.github.com>
2022-02-24 10:09:43 +0530

    implemented subtract fun from remote repository

--- math.py
+++ math.py
@@ -3,7 +3,7 @@ def add(x,y):
     return x+y
 #substraction implementation
 def sub(x,y):
-    pass
+    return x-y                   #from remote repository
 #multiplication implementation
 def mul(x,y):
     pass
